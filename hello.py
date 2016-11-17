76fb5956082b522a43c493abd8ff0014e620e09a
Partevikov <Partevikov@mail.ru>
2016-11-18 01:58:43 +1000

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,4 +8,4 @@ def myapp(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
-    return ['Hello World!\n']
+    return output
