eb0ed97f9d861815b7740c9cd1251c116d5c0f85
Partevikov <Partevikov@mail.ru>
2016-11-18 01:30:37 +1000

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+def myapp(environ, start_response):
+    d = parse_qsl(environ['QUERY_STRING'])
+    
+    output = ''
+    for key,value in d:
+                output.append(key+'='+value+'\n')
+
+    status = '200 OK'
+    response_headers = [('Content-type','text/plain')]
+    start_response(status, response_headers)
+    return ['Hello World!\n']
