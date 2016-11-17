b58066c93d23017c3ebbdb0b63f530869ec42ae2
Partevikov <Partevikov@mail.ru>
2016-11-18 02:05:58 +1000

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -s -r /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 
 sudo service nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app &
+sudo gunicorn -b 0.0.0.0:8080 -D hello.py:app &
