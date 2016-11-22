c3ed42961682197c2dfbfa657de801dc12983bb9
ISChetverikov <ischetverikov@gmail.com>
2016-11-22 22:20:00 +1000

    Поменял название у контроллера (reverted from commit 051d19a3dc5355c1e31fb3bd72bdfbec7276e5c6)

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-def application(request, *args, **kwargs):
+def test(request, *args, **kwargs):
     return HttpResponse('OK')
