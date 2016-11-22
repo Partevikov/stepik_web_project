9c33b31760f9a75832dee35a9d8011ea503d87df
ISChetverikov <ischetverikov@gmail.com>
2016-11-22 22:15:25 +1000

    Попытка подключить urls.py из ask

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from qa.views import  test
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
+    url(r'^question/$', test),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^question/', include(qa.urls)),
+    #url(r'^question/$', include(qa.urls)),
 )
