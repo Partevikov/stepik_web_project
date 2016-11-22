5deec3632757248b00bc1b499c572e1e78a08f98
ISChetverikov <ischetverikov@gmail.com>
2016-11-22 22:07:45 +1000

    Попытка настроить первое приложение

---
+++ ask/qa/urls.py
@@ -0,0 +1,9 @@
+from view.py import  test
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^(?P<num>\d+)/$', test),
+)
