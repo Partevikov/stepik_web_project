d150c7293fce2ca4b5967e1afce91c9ced262241
ISChetverikov <ischetverikov@gmail.com>
2016-11-22 19:26:01 +1000

    Перенос проекта в единую папку web (чтобы легче было разворачивать на терминале) Добавлены конфиги Gunicorn

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
 sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-
-#sudo rm -r /etc/gunicorn.d/hello.py
-#sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-
 sudo service nginx restart
+
+sudo rm -r /etc/gunicorn.d/test-wsgi
+sudo rm -r /etc/gunicorn.d/test-django
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
+
+
 #sudo gunicorn -с /etc/gunicorn.d/hello.py hello:app
-sudo gunicorn -b 0.0.0.0:8000 ask.qa:test &
+#sudo gunicorn -b 0.0.0.0:8000 ask.qa:test &
