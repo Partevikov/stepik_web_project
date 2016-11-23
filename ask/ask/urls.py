ef62107998b365a305c12ad56e6129650108cfdf
ISChetverikov <ischetverikov@gmail.com>
2016-11-23 13:59:28 +1000

    Добавлены новый URL

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -4,10 +4,16 @@ from django.contrib import admin
 from qa.views import test
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('qa.views',
     # Examples:
     # url(r'^question/$', test),
-    # url(r'^blog/', include('blog.urls')),
+    # url(r'^blog/', include('blog.urls')),/
 
-    url(r'^question/', 'qa.views.test'),
+    url(r'^$', test),
+    url(r'^login/', test),
+    url(r'^signup/', test),
+    url(r'^question/', test),
+    url(r'^popular/', test),
+    url(r'^ask/', test),
+    url(r'^new/', test),
 )
